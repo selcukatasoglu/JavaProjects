d9d522d2c7335ba498092809d2f28fcc0792c389
Selçuk Enes Ataşoğlu <selcukatasogluu@gmail.com>
2023-06-26 08:57:56 +0300

    Üstlü İfade Hesaplayan Program

---
+++ UsHesaplayanProgram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UsHesaplayanProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Tabanı girin: ");
+        int taban = scanner.nextInt();
+
+        System.out.print("Üssü girin: ");
+        int us = scanner.nextInt();
+
+        int sonuc = 1;
+        for (int i = 1; i <= us; i++) {
+            sonuc *= taban;
+        }
+
+        System.out.println("Sonuç: " + sonuc);
+    }
+}
