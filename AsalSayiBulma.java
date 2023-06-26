47f033064cba81df1117a2587e5c77a58aed4256
Selçuk Enes Ataşoğlu <selcukatasogluu@gmail.com>
2023-06-26 09:26:48 +0300

    Asal Sayı Bulan Program
    1 ile 100 arasındaki asal sayıları bulan program.

---
+++ AsalSayiBulma.java
@@ -0,0 +1,20 @@
+public class AsalSayiBulma {
+    public static void main(String[] args) {
+        System.out.println("1-100 arasındaki asal sayılar:");
+
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            boolean asalMi = true;
+
+            for (int i = 2; i <= Math.sqrt(sayi); i++) {
+                if (sayi % i == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            if (asalMi) {
+                System.out.print(sayi + " ");
+            }
+        }
+    }
+}
