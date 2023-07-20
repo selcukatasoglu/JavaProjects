4aeaafe338438bfe6313e6d7dda2a8b9f36d2276
Selçuk Enes Ataşoğlu <selcukatasogluu@gmail.com>
2023-07-20 14:40:55 +0300

    Boks Oyunu

--- Main.java
+++ Main.java
@@ -1,113 +1,108 @@
-class Course {
+class Fighter {
     String name;
-    String code;
-    String prefix;
-    double note;
-    Teacher teacher;
-    double oralPercentage; // Sözlü notunun ortalamaya etkileme yüzdesi
+    int damage;
+    int health;
+    int weight;
+    double dodge;
 
-    public Course() {
-    }
-
-    public Course(String name, String code, String prefix, double oralPercentage) {
+    public Fighter(String name, int damage, int health, int weight, double dodge) {
         this.name = name;
-        this.code = code;
-        this.prefix = prefix;
-        this.note = 0;
-        this.oralPercentage = oralPercentage;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        this.dodge = dodge;
     }
 
-    public void addTeacher(Teacher teacher) {
-        this.teacher = teacher;
+    public int hit(Fighter foe) {
+        System.out.println("------------");
+        System.out.println(this.name + " => " + foe.name + " " +  this.damage + " hasar vurdu.");
+
+        if (foe.dodge()) {
+            System.out.println(foe.name + " gelen hasarı savurdu.");
+            return foe.health;
+        }
+
+        if (foe.health - this.damage < 0)
+            return 0;
+
+        return foe.health - this.damage;
     }
 
-    public void printTeacher() {
-        System.out.println("Course: " + name + " Teacher: " + teacher.name);
+    public boolean dodge() {
+        double randomValue = Math.random() * 100;  //0.0 to 99.9
+        return randomValue <= this.dodge;
     }
 }
 
-class Teacher {
-    String name;
-    String mpno;
-    String branch;
 
-    public Teacher() {
+
+ class Ring {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
     }
 
-    public Teacher(String name, String mpno, String branch) {
-        this.name = name;
-        this.mpno = mpno;
-        this.branch = branch;
+    public void run() {
+
+        if (checkWeight()) {
+            while (f1.health > 0 && f2.health > 0) {
+                System.out.println("======== YENİ ROUND ===========");
+                f2.health = f1.hit(f2);
+                if (isWin()){
+                    break;
+                }
+                f1.health = f2.hit(f1);
+                if (isWin()){
+                    break;
+                }
+                printScore();
+            }
+
+        } else {
+            System.out.println("Sporcuların ağırlıkları uyuşmuyor.");
+        }
+
+
+    }
+
+    public boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight);
+    }
+
+    public boolean isWin() {
+        if (f1.health == 0) {
+            System.out.println("Maçı Kazanan : " + f2.name);
+            return true;
+        } else if (f2.health == 0){
+            System.out.println("Maçı Kazanan : " + f2.name);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void printScore() {
+        System.out.println("------------");
+        System.out.println(f1.name + " Kalan Can \t:" + f1.health);
+        System.out.println(f2.name + " Kalan Can \t:" + f2.health);
     }
 }
 
-class Student {
-    String name;
-    int stuNo;
-    String classes;
-    Course course1;
-    Course course2;
-    Course course3;
-    double average;
-    boolean isPass;
 
-    public Student() {
-    }
-
-    public Student(String name, int stuNo, String classes, Course course1, Course course2, Course course3) {
-        this.name = name;
-        this.stuNo = stuNo;
-        this.classes = classes;
-        this.course1 = course1;
-        this.course2 = course2;
-        this.course3 = course3;
-        this.average = 0.0;
-        this.isPass = false;
-    }
-
-    public void addBulkExamNote(int note1, int note2, int note3) {
-        // Sınav notu ile sözlü notunun hesaplanması
-        double course1Note = (note1 * (1 - course1.oralPercentage)) + (course1.note * course1.oralPercentage);
-        double course2Note = (note2 * (1 - course2.oralPercentage)) + (course2.note * course2.oralPercentage);
-        double course3Note = (note3 * (1 - course3.oralPercentage)) + (course3.note * course3.oralPercentage);
-
-        // Ders notlarının ortalamasının hesaplanması
-        this.average = (course1Note + course2Note + course3Note) / 3;
-
-        // Geçme notunun belirlenmesi (ortalama 55 ve üzeri ise geçer)
-        this.isPass = this.average >= 55;
-    }
-
-    public void printNote() {
-        System.out.println("Student Name: " + name);
-        System.out.println(course1.name + " Note: " + course1.note);
-        System.out.println(course2.name + " Note: " + course2.note);
-        System.out.println(course3.name + " Note: " + course3.note);
-        System.out.println("Avarage: " + average);
-        System.out.println("Pass: " + isPass);
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
-        Teacher mathTeacher = new Teacher("John Doe", "12345", "Mathematics");
-        Teacher physicsTeacher = new Teacher("Jane Smith", "67890", "Physics");
-
-        Course mathCourse = new Course("Math 101", "MATH101", "MATH", 0.15);
-        mathCourse.addTeacher(mathTeacher);
-        mathCourse.note = 80;
-
-        Course physicsCourse = new Course("Physics 101", "PHY101", "PHY", 0.20);
-        physicsCourse.addTeacher(physicsTeacher);
-        physicsCourse.note = 80;
-
-        Course chemistryCourse = new Course("Chemistry 101", "CHEM101", "CHEM", 0.10);
-        chemistryCourse.note = 80;
-
-        Student student = new Student("Alice", 123456, "Science",
-                mathCourse, physicsCourse, chemistryCourse);
-
-        student.addBulkExamNote(80, 80, 80);
-        student.printNote();
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
     }
 }
