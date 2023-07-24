a809dc5e11b6b11d27c1f140814053f7f92e9e2c
Selçuk Enes Ataşoğlu <selcukatasogluu@gmail.com>
2023-07-24 16:09:33 +0300

    Bir Matrisin Transpozunu Bulan Program

---
+++ TranspozBulma.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class TranspozBulma {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int[][] transpose = findTranspose(matrix);
+        printMatrix(transpose);
+    }
+
+    public static int[][] findTranspose(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transpose = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        return transpose;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+}
